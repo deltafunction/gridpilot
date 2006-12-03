f187e26962f4b4f6cee1cfbbc368d7f2d4e33b78
Frederik Orellana <frederik@orellana.dk>
2006-12-03 13:55:31 +0000

    update

--- build/gridpilot.sh
+++ build/gridpilot.sh
@@ -7,11 +7,5 @@ export mx="128m"
 jars=`ls lib/*.jar`
 classpath=`echo $jars | sed 's/ /:/g'`
 
-#java -cp $classpath:gridpilot.signed.jar \
-#-Djava.security.manager -Djava.security.policy=suresh.policy \
-#-Dmessagefile=null -Xms$ms -Xmx$mx gridpilot.GridPilot 
-
-java -cp $classpath:gridpilot.jar -Dmessagefile=null -Xms$ms -Xmx$mx \
+java -cp `ls gridpilot*.jar`:$classpath -Dmessagefile=null -Xms$ms -Xmx$mx \
 gridpilot.GridPilot 
-
-#java -cp .:$classpath -Dmessagefile=null -Xms$ms -Xmx$mx gridpilot.GridPilot 
