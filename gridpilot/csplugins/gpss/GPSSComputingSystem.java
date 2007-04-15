8f5c4ea4170c1b7a51649cf4418f15d14934c874
Frederik Orellana <frederik@orellana.dk>
2007-04-15 09:09:51 +0000

    Working on GPSS.

--- gridpilot/csplugins/gpss/GPSSComputingSystem.java
+++ gridpilot/csplugins/gpss/GPSSComputingSystem.java
@@ -3,6 +3,8 @@ package gridpilot.csplugins.gpss;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Vector;
@@ -21,7 +23,7 @@ import gridpilot.Debug;
 import gridpilot.GridPilot;
 import gridpilot.JobInfo;
 import gridpilot.LogFile;
-import gridpilot.Util;
+import gridpilot.PullJobsDaemon;
 
 public class GPSSComputingSystem implements ComputingSystem{
 
@@ -31,6 +33,8 @@ public class GPSSComputingSystem implements ComputingSystem{
   private String remoteDB = null;
   private String csName;
   private HashSet finalRuntimesLocal = null;
+  private String user = null;
+
   
   // RTEs are refreshed from entries written by pull nodes in remote database
   // every RTE_SYNC_DELAY milliseconds.
@@ -52,50 +56,227 @@ public class GPSSComputingSystem implements ComputingSystem{
     remoteDB = GridPilot.getClassMgr().getConfigFile().getValue(
         csName, "remote database");
     timerSyncRTEs.setDelay(RTE_SYNC_DELAY);
+    // Set user
+    try{
+      Debug.debug("getting credential", 3);
+      GSSCredential credential = GridPilot.getClassMgr().getGridCredential();
+      GlobusCredential globusCred = null;
+      if(credential instanceof GlobusGSSCredentialImpl){
+        globusCred = ((GlobusGSSCredentialImpl)credential).getGlobusCredential();
+      }
+      Debug.debug("getting identity", 3);
+      user = globusCred.getIdentity();
+      /* remove leading whitespace */
+      user = user.replaceAll("^\\s+", "");
+      /* remove trailing whitespace */
+      user = user.replaceAll("\\s+$", "");      
+    }
+    catch(Exception ioe){
+      error = "Exception during getUserInfo\n" +
+      "\tException\t: " + ioe.getMessage();
+      logFile.addMessage(error, ioe);
+    }
   }
 
   /**
-   * Copies local copy of the corresponding jobDefinition plus
-   * the associated dataset to the (remote) database, where it
-   * will be picked up by pull clients. Sets csState to 'ready'.
+   * Copies jobDefinition plus the associated dataset to the (remote) database,
+   * where it will be picked up by pull clients. Sets csState to 'ready'.
+   * Sets finalStdOut and finalStdErr and any local output files 
+   * to files in the remote gridftp directory.
    */
   public boolean submit(JobInfo job){
-    DBRecord jobRecord = null;
+    DBRecord jobDefinition = null;
     DBRecord dataset = null;
     String datasetID = null;
     DBRecord transformation = null;
+    String transformationName = null;
+    String transformationVersion = null;
     String transformationID = null;
-    DBPluginMgr jobMgr = GridPilot.getClassMgr().getDBPluginMgr(
-        job.getDBName());
+    DBPluginMgr jobDbMgr = GridPilot.getClassMgr().getDBPluginMgr(job.getDBName());
     DBPluginMgr remoteMgr = GridPilot.getClassMgr().getDBPluginMgr(
         remoteDB);
     // First, read the jobDefinition, dataset and transformation.
     try{
-      String [] jobDefDatasetReference = Util.getJobDefDatasetReference(job.getDBName());  
-      jobRecord = jobMgr.getJobDefinition(job.getJobDefId());
-      datasetID = (String) jobRecord.getValue(jobDefDatasetReference[1]);
-      dataset = jobMgr.getDataset(datasetID);
+      jobDefinition = jobDbMgr.getJobDefinition(job.getJobDefId());
+      datasetID = jobDbMgr.getJobDefDatasetID(job.getJobDefId());
+      dataset = jobDbMgr.getDataset(datasetID);
+      transformationName = jobDbMgr.getDatasetTransformationName(datasetID);
+      transformationVersion = jobDbMgr.getDatasetTransformationVersion(datasetID);
+      transformationID = jobDbMgr.getTransformationID(transformationName, transformationVersion);
+      transformation = jobDbMgr.getTransformation(transformationID);
+      // Hmm, 7 database lookups. Perhaps we should reconsider this...
     }
     catch(Exception e){
-      error = "ERROR: could not read dataset or jobDefinition. "+e.getMessage();
+      error = "ERROR: could not read jobDefinition, dataset or transformation. "+e.getMessage();
       return false;
     }
-    // Now, create the transformation if necessary
     try{
-      remoteMgr.createDataset("dataset", dataset.fields, dataset.values);
+      // Create the transformation if necessary
+      transformationID = remoteMgr.getTransformationID(transformationName, transformationVersion);
+      if(transformationID==null || transformationID.equals("-1")){
+        remoteMgr.createTrans(transformation.fields, transformation.values);
+      }
+      // Tag it for deletion
+      tagDeleteTransformation(remoteMgr, transformationID);
+      // Create the dataset if necessary
+      String datasetName = null;
+      try{
+        datasetName = remoteMgr.getDatasetName(datasetID);
+      }
+      catch(Exception ee){
+      }
+      if(datasetName==null || datasetName.equals("")){
+        remoteMgr.createDataset("dataset", dataset.fields, dataset.values);
+      }
+      // Tag it for deletion
+      tagDeleteDataset(remoteMgr, datasetID);
+      // Modify and write the jobDefinition
+      jobDefinition.setValue("csStatus", PullJobsDaemon.STATUS_READY);
+      jobDefinition = updateURLs(jobDefinition);
+      remoteMgr.createJobDef(jobDefinition.fields, jobDefinition.values);
     }
     catch(Exception e){
-      error = "ERROR: could not write dataset or jobDefinition. "+e.getMessage();
+      error = "ERROR: could not write to 'remote' database. "+e.getMessage();
       return false;
     }
-    
     return true;
+  }
+  
+  private void getRemoteDir(DBRecord jobDefinition){
+    // TODO Auto-generated method stub
+    
+  }
+  
+  private DBRecord updateURLs(DBRecord jobDefinition){
+    // TODO
+    try{
+      getRemoteDir(jobDefinition);
+    }
+    catch(Exception e){
+      error = "ERROR: could not set remote output files. "+e.getMessage();
+     }
+    return jobDefinition;
+  }
+
+  /**
+   * Tags a transformation record, copied to a remote database only to be
+   * able to run a job, for deletion once the job has finished.
+   * If the transformation happens to be used by other GPSS jobs, this is
+   * taken into account - i.e. it is not deleted until the last one has finished.
+   */
+  private void tagDeleteTransformation(DBPluginMgr remoteMgr, String transformationID){
+    String comment = (String) remoteMgr.getTransformation(transformationID).getValue("comment");
+    if(comment!=null && comment.startsWith("volatile:")){
+      if(!comment.matches("volatile::'"+user+"'.*") &&
+          !comment.matches("volatile::.*::'"+user+"'.*")){
+        comment = comment+"::'"+user+"'";
+      }
+    }
+    else{
+      comment = "volatile::'"+user+"'";
+    }
+  }
+
+  /**
+   * Deletes transformation tagged for deletion.
+   * If a transformation happens to be used by other GPSS jobs, this is
+   * taken into account - i.e. it is not deleted until the last one has finished.
+   */
+  private void deleteTaggedTransformation(DBPluginMgr remoteMgr, String transformationID){
+    String comment = (String) remoteMgr.getTransformation(transformationID).getValue("comment");
+    if(comment!=null && comment.startsWith("volatile:")){
+      if(!comment.matches("volatile::'"+user+"'")){
+        remoteMgr.deleteTransformation(transformationID);
+      }
+      else{
+        comment = comment.replaceFirst("::'"+user+"'", "");
+        remoteMgr.updateTransformation(transformationID, new String [] {"comment"}, new String [] {comment});
+      }
+    }
+  }
+
+  /**
+   * Tags a dataset record, copied to a remote database only to be
+   * able to run a job, for deletion once the job has finished.
+   * If the dataset happens to be used by other GPSS jobs, this is
+   * taken into account - i.e. it is not deleted until the last one has finished.
+   */
+  private void tagDeleteDataset(DBPluginMgr remoteMgr, String datasetID){
+    String metaData = (String) remoteMgr.getDataset(datasetID).getValue("metaData");
+    if(metaData!=null && metaData.startsWith("volatile:")){
+      if(!metaData.matches("volatile::'"+user+"'.*") &&
+          !metaData.matches("volatile::.*::'"+user+"'.*")){
+        metaData = metaData+"::'"+user+"'";
+      }
+    }
+    else{
+      metaData = "volatile::'"+user+"'";
+    }
+  }
+
+  /**
+   * Deletes transformation tagged for deletion.
+   * If a transformation happens to be used by other GPSS jobs, this is
+   * taken into account - i.e. it is not deleted until the last one has finished.
+   */
+  private void deleteTaggedDataset(DBPluginMgr remoteMgr, String datasetID){
+    String metaData = (String) remoteMgr.getDataset(datasetID).getValue("metaData");
+    if(metaData!=null && metaData.startsWith("volatile:")){
+      if(!metaData.matches("volatile::'"+user+"'")){
+        remoteMgr.deleteDataset(datasetID, false);
+      }
+      else{
+        metaData = metaData.replaceFirst("::'"+user+"'", "");
+        remoteMgr.updateDataset(datasetID, new String [] {"metaData"}, new String [] {metaData});
+      }
+    }
   }
 
   /**
    * Sets csState to 'requestKill' in remote jobDefinition record.
    */
   public boolean killJobs(Vector jobs){
+    boolean ok = true;
+    // Group the jobs by DB
+    HashMap jobsMap = new HashMap();
+    Enumeration en = jobs.elements();
+    JobInfo job = null;
+    while(en.hasMoreElements()){
+      job = (JobInfo) en.nextElement();
+      if(!jobsMap.containsKey(job.getDBName())){
+        jobsMap.put(job.getDBName(), new Vector());
+      }
+      ((Vector) jobsMap.get(job.getDBName())).add(job);
+    }
+    Vector dbJobs = null;
+    DBPluginMgr mgr = null;
+    String dbName = null;
+    // Update each job
+    for(Iterator it=jobsMap.keySet().iterator(); it.hasNext();){
+      dbName = (String) it.next();
+      mgr = GridPilot.getClassMgr().getDBPluginMgr(dbName);
+      dbJobs = (Vector) jobsMap.get(dbName);
+      en = dbJobs.elements();
+      while(en.hasMoreElements()){
+        job = (JobInfo) en.nextElement();
+        ok = ok && mgr.updateJobDefinition(job.getJobDefId(),
+            new String [] {"csState"}, new String [] {PullJobsDaemon.STATUS_REQUESTED_KILLED});
+      }
+    }
+    return ok;
+  }
+
+  /**
+   * Creates 'local' copy of corresponding jobDefinition. For this copy:
+   * If finalStdout, finalStderr or any output files are local:
+   * - Creates temporary directory on the configured gridftp server.
+   * - Uploads any local input files and sets inputFiles accordingly.
+   */
+  public boolean preProcess(JobInfo job){
+    
+    
+    
+    
     // TODO
     return false;
   }
@@ -105,6 +286,26 @@ public class GPSSComputingSystem implements ComputingSystem{
    */
   public void clearOutputMapping(JobInfo job){
     // TODO
+  }
+
+  /**
+   * - Downloads any files specified as local in the original jobDefinition
+   *   from the remote temporary location.
+   * - Deletes the remote temporary location.
+   * - Cleans up temporary transformation and dataset if any such was written.
+   */
+  public boolean postProcess(JobInfo job){
+    // TODO
+    
+    // Clean up temporary transformation and dataset
+    DBPluginMgr remoteMgr = GridPilot.getClassMgr().getDBPluginMgr(job.getDBName());
+    String datasetID = remoteMgr.getJobDefDatasetID(job.getJobDefId());
+    String transformationName = remoteMgr.getDatasetTransformationName(datasetID);
+    String transformationVersion = remoteMgr.getDatasetTransformationVersion(datasetID);
+    String transformationID = remoteMgr.getTransformationID(transformationName, transformationVersion);
+    deleteTaggedDataset(remoteMgr, datasetID);
+    deleteTaggedTransformation(remoteMgr, transformationID);
+    return false;
   }
 
   /**
@@ -127,33 +328,6 @@ public class GPSSComputingSystem implements ComputingSystem{
     return null;
   }
 
-  /**
-   * Creates 'local' copy of corresponding jobDefinition. For this copy:
-   * If finalStdout, finalStderr or any output files are local:
-   * - Creates temporary directory on the configured gridftp server.
-   * - Uploads any local input files and sets inputFiles accordingly.
-   * - Sets finalStdOut and finalStdErr and any local output files 
-   *   to files in this directory.
-   */
-  public boolean preProcess(JobInfo job){
-    // TODO
-    return false;
-  }
-
-  /**
-   * - Downloads any files specified as local in the original jobDefinition
-   *   from the remote temporary location.
-   * - Deletes the remote temporary location.
-   */
-  public boolean postProcess(JobInfo job){
-    // TODO
-    return false;
-  }
-
-  /**
-   * Checks for requested jobs and sets permissions if some
-   * are detected.
-   */
   public void updateStatus(Vector jobs){
     // TODO
   }
@@ -168,26 +342,6 @@ public class GPSSComputingSystem implements ComputingSystem{
   }
 
   public String getUserInfo(String csName){
-    String user = null;
-    try{
-      Debug.debug("getting credential", 3);
-      GSSCredential credential = GridPilot.getClassMgr().getGridCredential();
-      GlobusCredential globusCred = null;
-      if(credential instanceof GlobusGSSCredentialImpl){
-        globusCred = ((GlobusGSSCredentialImpl)credential).getGlobusCredential();
-      }
-      Debug.debug("getting identity", 3);
-      user = globusCred.getIdentity();
-      /* remove leading whitespace */
-      user = user.replaceAll("^\\s+", "");
-      /* remove trailing whitespace */
-      user = user.replaceAll("\\s+$", "");      
-    }
-    catch(Exception ioe){
-      error = "Exception during getUserInfo\n" +
-      "\tException\t: " + ioe.getMessage();
-      logFile.addMessage(error, ioe);
-    }
     return user;
   }
 
