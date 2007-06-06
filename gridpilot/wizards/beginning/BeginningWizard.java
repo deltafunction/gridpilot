687c7bbba76038b378fe6744bf66c097f1c283a0
Frederik Orellana <frederik@orellana.dk>
2007-06-06 13:28:08 +0000

    Working on wizards.

--- gridpilot/wizards/beginning/BeginningWizard.java
+++ gridpilot/wizards/beginning/BeginningWizard.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -22,6 +24,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class BeginningWizard{
@@ -29,7 +32,9 @@ public class BeginningWizard{
   private ImageIcon icon = null;
   private ConfigFile configFile = null;
   private boolean changes = false;
+  private JRadioButton [] jtbs = null;
   private static int TEXTFIELDWIDTH = 32;
+  private static String HOMEGRID_URL = "https://homegrid.dyndns.org/";
 
   public BeginningWizard(boolean firstRun){
     
@@ -73,8 +78,10 @@ public class BeginningWizard{
         }
       }
       
-      // TODO: set grid homedir
-      
+      if(setGridHomeDir(firstRun)==2){
+        return;
+      }
+
       // TODO: Set grid file catalog - fallback to homegrid.dyndns.org
       
       // TODO: Set grid job DB - fallback to homegrid.dyndns.org
@@ -126,7 +133,7 @@ public class BeginningWizard{
     confirmLabel.setText(confirmString);
     int choice = -1;
     choice = confirmBox.getConfirm("Starting with GridPilot",
-        confirmString, new Object[] {"Continue", "Cancel"}, icon, Color.WHITE);
+        confirmString, new Object[] {"Continue", "Cancel"}, icon, Color.WHITE, false);
     return choice;
   }
   
@@ -141,7 +148,7 @@ public class BeginningWizard{
             (firstRun?"\n\nThanks for using GridPilot and have fun!":"");
     int choice = -1;
     confirmBox.getConfirm("Setup completed!",
-        confirmString, new Object[] {"OK",  "Cancel"}, icon, Color.WHITE);   
+        confirmString, new Object[] {"OK",  "Cancel"}, icon, Color.WHITE, false);   
     return choice;
   }
   
@@ -149,7 +156,7 @@ public class BeginningWizard{
     ConfirmBox confirmBox = new ConfirmBox(JOptionPane.getRootFrame());
     String confirmString = "ERROR: could not set up GridPilot. "+text;
     confirmBox.getConfirm("Failed setting up GridPilot",
-        confirmString, new Object[] {"OK"}, icon, Color.WHITE);
+        confirmString, new Object[] {"OK"}, icon, Color.WHITE, false);
   }
   
   
@@ -201,10 +208,10 @@ public class BeginningWizard{
       dbDir.replaceFirst("hsql://localhost/", "");
       dbDir.replaceFirst("/My_DB", "");
     }
-    String cacheDir = configFile.getValue("GridPilot", "pull cache directory");
-    String workingDir = configFile.getValue("Fork", "working directory");
-    String runtimeDir = configFile.getValue("Fork", "runtime directory");
-    String transDir = configFile.getValue("Fork", "transformation directory");
+    String cacheDir = configFile.getValue("GridPilot", "Pull cache directory");
+    String workingDir = configFile.getValue("Fork", "Working directory");
+    String runtimeDir = configFile.getValue("Fork", "Runtime directory");
+    String transDir = configFile.getValue("Fork", "Transformation directory");
     String [] defDirs = new String [] {
         dbDir==null?dbDir:"~/GridPilot",
         cacheDir==null?dbDir+"/cache":cacheDir,
@@ -230,8 +237,7 @@ public class BeginningWizard{
       jtFields[i] = new JTextField(TEXTFIELDWIDTH);
       jtFields[i].setText(defDirs[i]);
       row = new JPanel(new BorderLayout());
-      row.add(Util.createCheckPanel(
-          JOptionPane.getRootFrame(),
+      row.add(Util.createCheckPanel(JOptionPane.getRootFrame(),
           names[i], jtFields[i]), BorderLayout.WEST);
       subRow = new JPanel(new BorderLayout());
       subRow.add(jtFields[i], BorderLayout.CENTER);
@@ -248,7 +254,7 @@ public class BeginningWizard{
     int choice = -1;
     try{
       choice = confirmBox.getConfirm("Setting up GridPilot directories",
-          jPanel, new Object[] {"Continue", "Skip", "Cancel"}, icon, Color.WHITE);
+          jPanel, new Object[] {"Continue", "Skip", "Cancel"}, icon, Color.WHITE, false);
     }
     catch(Exception e){
       e.printStackTrace();
@@ -322,7 +328,8 @@ public class BeginningWizard{
       "credentials (proxies).\n\n" +
       "Optionally, you can also specify a directory with the certificates of the\n" +
       "certificate authories (CAs) that you trust. This can safely be left unspecified\n" +
-      "in which case a default set of CAs will be trusted.";
+      "in which case a default set of CAs will be trusted.\n\n" +
+      "Specified, but non-existing directories will be created.\n\n";
     JPanel jPanel = new JPanel(new GridBagLayout());
     String certPath = configFile.getValue("GridPilot", "key file");
     String keyPath = configFile.getValue("GridPilot", "certificate file");
@@ -351,8 +358,7 @@ public class BeginningWizard{
       jtFields[i] = new JTextField(TEXTFIELDWIDTH);
       jtFields[i].setText(defDirs[i]);
       row = new JPanel(new BorderLayout());
-      row.add(Util.createCheckPanel(
-          JOptionPane.getRootFrame(),
+      row.add(Util.createCheckPanel(JOptionPane.getRootFrame(),
           names[i], jtFields[i]), BorderLayout.WEST);
       subRow = new JPanel(new BorderLayout());
       subRow.add(jtFields[i], BorderLayout.CENTER);
@@ -369,7 +375,7 @@ public class BeginningWizard{
     int choice = -1;
     try{
       choice = confirmBox.getConfirm("Setting up grid credentials",
-          jPanel, new Object[] {"Continue", "Skip", "Cancel"}, icon, Color.WHITE);
+          jPanel, new Object[] {"Continue", "Skip", "Cancel"}, icon, Color.WHITE, false);
     }
     catch(Exception e){
       e.printStackTrace();
@@ -417,8 +423,8 @@ public class BeginningWizard{
         newDirs[3]!=null && (defDirs[3]==null || !defDirs[3].equals(newDirs[3]))){
       configFile.setAttributes(
           new String [] {"GridPilot", "GridPilot", "GridPilot", "GridPilot"},
-          new String [] {"key file", "certificate file", "grid proxy directory",
-              "ca certificates"},
+          new String [] {"Key file", "Certificate file", "Grid proxy directory",
+              "CA certificates"},
           new String [] {
               newDirs[0], newDirs[1], newDirs[2], newDirs[3]}
       );
@@ -427,8 +433,133 @@ public class BeginningWizard{
 
     return choice;
   }
+
+  private int setGridHomeDir(boolean firstRun) throws Exception{
+    String confirmString =
+      "When running jobs on a grid it is useful to have the jobs upload\n" +
+      "output files to a directory on a server that's always on.\n\n" +
+      "For this to be possible GridPilot needs to know a gridftp or\n" +
+      "https URL where you have read/write permission with the X509\n" +
+      "certificate you specified previously.\n\n" +
+      "If you don't know any such URL or you don't understand the above,\n" +
+      "you may use the default grid home URL given below. But please\n" +
+      "notice that this is but a temporary solution and that the files on\n" +
+      "this location may be read, overwritten or deleted by others at any time.\n\n"+
+      "You may also choose a local directory, but in this case, output files\n" +
+      "will stay on the resource where a job has run until GridPilot\n" +
+      "downloads them.\n\n"+
+      "A specified, local, but non-existing directory will be created.\n\n";
+    JPanel jPanel = new JPanel(new GridBagLayout());
+    String homeUrl = configFile.getValue("GridPilot", "Grid home url");
+    String [] defDirs = new String [] {homeUrl,
+                                       HOMEGRID_URL+Util.getGridDatabaseUser1()+"/",
+                                       homeUrl};
+    String [] names = new String [] {"Use your own grid or local home URL",
+                                     "Use default grid home URL",
+                                     "Use default local home URL"};
+    JTextField [] jtFields = new JTextField [defDirs.length];
+    jPanel.add(new JLabel("<html>"+confirmString.replaceAll("\n", "<br>")+"</html>"),
+        new GridBagConstraints(0, (firstRun?1:0), 2, 2, 0.0, 0.0,
+            GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+            new Insets(5, 5, 5, 5), 0, 0));
+    JPanel row = null;
+    JPanel subRow = null;
+    jtbs = new JRadioButton[defDirs.length];
+    RadioListener myListener = new RadioListener();
+    for(int i=0; i<defDirs.length; ++i){
+      jtFields[i] = new JTextField(TEXTFIELDWIDTH);
+      jtFields[i].setText(defDirs[i]);
+      jtbs[i] = new JRadioButton();
+      jtbs[i].addActionListener(myListener);
+      row = new JPanel(new BorderLayout());
+      row.add(jtbs[i], BorderLayout.WEST);
+      if(i==0){
+        row.add(Util.createCheckPanel(JOptionPane.getRootFrame(),
+            names[i], jtFields[i]), BorderLayout.CENTER);
+      }
+      else{
+        row.add(new JLabel(names[i]), BorderLayout.CENTER);
+        jtFields[i].setEditable(false);
+      }
+      subRow = new JPanel(new BorderLayout());
+      subRow.add(jtFields[i], BorderLayout.CENTER);
+      subRow.add(new JLabel("   "), BorderLayout.SOUTH);
+      subRow.add(new JLabel("   "), BorderLayout.NORTH);
+      row.add(subRow, BorderLayout.EAST);
+      jPanel.add(row, new GridBagConstraints(0, i+(firstRun?5:4), 1, 1, 0.0, 0.0,
+          GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+          new Insets(0, 0, 0, 0), 0, 0));
+    }
+    jPanel.validate();
+    
+    ConfirmBox confirmBox = new ConfirmBox(JOptionPane.getRootFrame());
+    int choice = -1;
+    try{
+      choice = confirmBox.getConfirm("Setting up home directory",
+          jPanel, new Object[] {"Continue", "Skip", "Cancel"}, icon, Color.WHITE, false);
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+    
+    if(choice!=0){
+      return choice;
+    }
+  
+    // Create missing directories
+    String [] newDirs = new String[defDirs.length];
+    File [] newDirFiles = new File[newDirs.length];
+    int sel = -1;
+    for(int i=0; i<newDirs.length; ++i){
+      if(jtbs[i].isSelected()){
+        sel = i;
+      }
+      else{
+        continue;
+      }
+      newDirs[i] = jtFields[i].getText();
+      if(newDirs[i]!=null && !newDirs[i].equals("") && !Util.urlIsRemote(newDirs[i])){
+        Debug.debug("Checking directory "+newDirs[i], 2);
+        newDirFiles[i] = new File(Util.clearTildeLocally(Util.clearFile(newDirs[i])));
+        if(newDirFiles[i].exists()){
+          if(!newDirFiles[i].isDirectory()){
+            throw new IOException("The directory "+newDirFiles[i].getCanonicalPath()+" cannot be created.");
+          }
+        }
+        else{
+          Debug.debug("Creating directory "+newDirs[i], 2);
+          newDirFiles[i].mkdir();
+        }
+      }
+      newDirs[i] = Util.replaceWithTildeLocally(Util.clearFile(newDirs[i]));
+    }
+      
+    // Set config entries
+    if(sel>-1 && newDirs[sel]!=null &&
+        (defDirs[0]==null || !defDirs[0].equals(newDirs[sel]))){
+      Debug.debug("Setting "+sel+":"+newDirs[sel], 2);
+      configFile.setAttributes(
+          new String [] {"GridPilot"},
+          new String [] {"Grid home url"},
+          new String [] {newDirs[sel]}
+      );
+      changes = true;
+    }
+  
+    return choice;
+  }
   
 
-  
+  /** Listens to the radio buttons. */
+  class RadioListener implements ActionListener { 
+      public void actionPerformed(ActionEvent e) {
+        for(int i=0; i<jtbs.length; ++i){
+          if(e.getSource().equals(jtbs[i])){
+            continue;
+          }
+          jtbs[i].setSelected(false);
+        }
+      }
+  }
   
 }
