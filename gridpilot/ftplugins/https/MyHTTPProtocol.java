9764fbda6b0c509c43488d503a61a63a590439f2
Frederik Orellana <frederik@orellana.dk>
2007-06-07 21:26:27 +0000

    Implemented support for listing files.

--- gridpilot/ftplugins/https/MyHTTPProtocol.java
+++ gridpilot/ftplugins/https/MyHTTPProtocol.java
@@ -3,12 +3,18 @@ package gridpilot.ftplugins.https;
 import org.globus.util.http.HTTPProtocol;
 
 public class MyHTTPProtocol extends HTTPProtocol {
+  
+  private final static String DEPTH = "1";
+  
   public static String createCmdHeader(String path, String host, String cmd) {
     String newPath = null;
     
     newPath = "/" + path;    
     StringBuffer head = new StringBuffer();
     head.append(cmd+" " + newPath + " " + HTTPProtocol.HTTP_VERSION + HTTPProtocol.CRLF);
+    if(cmd.equalsIgnoreCase("propfind")){
+      head.append("Depth: " + DEPTH + HTTPProtocol.CRLF);
+    }
     head.append(HTTPProtocol.HOST + host + HTTPProtocol.CRLF);
     head.append(HTTPProtocol.CONNECTION_CLOSE);
     head.append(USER_AGENT + USER_AGENT + HTTPProtocol.CRLF);
