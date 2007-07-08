c62468e3f9259bbe780873a03eb5e65c237d627f
Frederik Orellana <frederik@orellana.dk>
2007-07-08 21:11:38 +0000

    Cleanup.

--- gridpilot/Util.java
+++ gridpilot/Util.java
@@ -76,7 +76,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
-import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
