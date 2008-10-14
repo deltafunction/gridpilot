a570bac32af1dfa66c45d08e790b2226e5b93295
Frederik Orellana <frederik@orellana.dk>
2008-10-14 11:36:40 +0000

    update

--- build/extract.sh
+++ build/extract.sh
@@ -5,7 +5,7 @@ cp -r ../gridpilot ./
 cp -r ../lib ./
 rm -rf lib/CVS
 cp ../gridpilot.conf ./
-ls resources/certificates > resources/ca_certs_list.txt
+#ls resources/certificates > resources/ca_certs_list.txt
 sed -e "s/.*date.*/\<\!--date--\>`date`/" resources/about.htm > tmpAbout.htm
 mv -f tmpAbout.htm resources/about.htm
 
