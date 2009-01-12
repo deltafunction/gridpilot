8d0f9a85a902f8fc14ed4d2f61dd73e280c33771
Frederik Orellana <frederik@orellana.dk>
2009-01-12 12:09:39 +0000

    MySQL example update script. This is to serve as inspiration on how to manage DB access.

---
+++ documentation/update_mysql_accounts.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+#
+# update_mysql_accounts.sh
+#
+# Script to synchronize the mysql access rights with virtual organization.
+# All users get a database created, carrying the name of their certificate
+# subject (with / replaced by | and spaces with _). Their login name is
+# the cksum of their certificate (printf "$subject" | cksum).
+# The database is world readable and modifyable by the user.
+#
+# Frederik Orellana, Geneva, September 2006
+#
+
+VO_URL="https://www.gridfactory.org/vos/db_users.txt"
+ROOT_PASSWORD=""
+# Set if you have local catalog(s)
+LOCAL_REPLICAS=""
+LOCAL_CATALOG=""
+
+#vo=`curl $VO_URL | grep Orellana`
+vo=`curl $VO_URL 2>/dev/null`
+
+# first some necessary entries
+# INSERT INTO db VALUES ('%', '', '', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N');
+# INSERT INTO db VALUES ('%', 'localreplicas', 'dq2user', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N');
+# INSERT INTO db VALUES ('%', 'atlas_ch', 'dq2user', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N');
+
+echo
+echo "----------------- new sync ---------------"
+echo
+
+echo $vo | sed 's/\" \"/\n/g' | sed 's/\"//g' | sed 's| \/|\n/|g' | while read subject
+do
+  dbName=`printf "$subject" | tr " " "_" | tr "/" "|" | tr "@" "_" | tr "." "_" | sed 's/\"//g' | sed 's/^|//g'`
+  dbName=`echo ${dbName:0:64}`
+  userHash=`printf "$subject" | cksum | awk '{print $1}'`
+  echo "subject: $subject"
+  echo "user hash: $userHash"
+  # first the user databases
+  echo "db: $dbName"
+  mysql -uroot -p$ROOT_PASSWORD $dbName -e"SHOW TABLES;" >&/dev/null
+  if [ $? -ne 0 ]; then
+    echo "database $dbName not found, creating..."
+    mysql -uroot -p$ROOT_PASSWORD -e"CREATE DATABASE \"$dbName\";"
+    echo "creating and granting privileges to user $userHash"
+    mysql -uroot -p$ROOT_PASSWORD -e"GRANT ALL ON \"$dbName\".* TO '$userHash'@'%' REQUIRE SUBJECT '$subject';"
+    # comment this out if you don't want world readable databases
+    mysql -uroot -p$ROOT_PASSWORD -e"INSERT INTO mysql.db (host,db,select_priv) VALUES('%','$dbName','y');"
+    if [ "$LOCAL_REPLICAS" != "" ]; then
+      # now the virtual file catalog 'localreplicas' (synchronized with fzk)
+      mysql -uroot -p$ROOT_PASSWORD $LOCAL_REPLICAS -e"SHOW TABLES;" >&/dev/null
+      if [ $? -eq 0 ]; then
+        echo "granting privileges to user $userHash on $LOCAL_REPLICAS"
+        mysql -uroot -p$ROOT_PASSWORD -e"GRANT ALL ON \"$LOCAL_REPLICAS\".* TO '$userHash'@'%' REQUIRE SUBJECT '$subject';"
+      fi
+    fi
+    if [ "$LOCAL_CATALOG" != "" ]; then
+      # now the  shared file catalog 'atlas_ch'
+      mysql -uroot -p$ROOT_PASSWORD $LOCAL_CATALOG -e"SHOW TABLES;" >&/dev/null
+      if [ $? -eq 0 ]; then
+        echo "granting privileges to user $userHash on $LOCAL_CATALOG"
+        mysql -uroot -p$ROOT_PASSWORD -e"GRANT ALL ON \"$LOCAL_CATALOG\".* TO '$userHash'@'%' REQUIRE SUBJECT '$subject';"
+      fi
+    fi
+    echo
+    echo
+  fi
+done
+mysql -uroot -p$ROOT_PASSWORD -e"FLUSH PRIVILEGES;"
